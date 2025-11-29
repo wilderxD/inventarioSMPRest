610ee9300784f52db6909707ad9da394a986893c
wilderxD <wilder1093@gmail.com>
2025-11-29 14:30:28 -0500

    cooom

--- src/main/java/com/grupoUTP/inventarioSMP/controller/AsignadoController.java
+++ src/main/java/com/grupoUTP/inventarioSMP/controller/AsignadoController.java
@@ -1,8 +1,15 @@
 package com.grupoUTP.inventarioSMP.controller;
 
+import com.grupoUTP.inventarioSMP.entity.Asignado;
 import com.grupoUTP.inventarioSMP.service.IAsignadoService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AsignadoController {
     
     private final IAsignadoService asignadoService;
+    
+    @GetMapping("/asignados")
+    public List<Asignado> listarAsignados(){
+        return asignadoService.findAll();
+    }
+    
+    @GetMapping("/asignados/page/{page}")
+    public Page<Asignado> listarAsignados(@PathVariable Integer page){
+        Pageable pageable = PageRequest.of(page, 5);
+        return asignadoService.findAll(pageable);
+    }
+    
+    
+    
 }
+
--- src/main/java/com/grupoUTP/inventarioSMP/service/AsignadoServiceImpl.java
+++ src/main/java/com/grupoUTP/inventarioSMP/service/AsignadoServiceImpl.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.grupoUTP.inventarioSMP.service;
 
-/**
- *
- * @author FUSER011
- */
-public class AsignadoServiceImpl {
+
+public class AsignadoServiceImpl extends IAsignadoService{
     
 }
